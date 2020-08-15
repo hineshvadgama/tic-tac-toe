25167bf85a634e64cc8b54c356918dd22ca4f79c
hineshvadgama <hinesh.vadgama@outlook.com>
2020-08-15 21:20:32 +0100

    conditional styling to winning modal

--- src/components/WinnerModal/WinnerModal.js
+++ src/components/WinnerModal/WinnerModal.js
@@ -3,15 +3,18 @@ import './WinnerModal.css';
 
 const WinnerModal = (props) => {
 
+    const classColour = props.winner === 'Player One' ? 'green' : 'pink';
+    const style = {backgroundColor: props.winner === 'Player One' ? '##4bfa4b' : '#ff2bea'}
+
     return (
 
         <div className="winner-modal-container">
 
-            <h1 className="green">Player One Wins!</h1>
+            <h1 className={classColour}>{props.winner} Wins!</h1>
             
             <img src="../../img/trophy.png" />
 
-            <button id="winner-modal-button">Play again!</button>
+            <button id="winner-modal-button" style={style}>Play again!</button>
 
         </div>
 

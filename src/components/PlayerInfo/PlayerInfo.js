27dcb489b7f50074795bfadcdb461d4f9d81fbd6
hineshvadgama <hinesh.vadgama@outlook.com>
2020-08-15 21:43:02 +0100

    style improvements

--- src/components/PlayerInfo/PlayerInfo.js
+++ src/components/PlayerInfo/PlayerInfo.js
@@ -6,7 +6,7 @@ const PlayerInfo = (props) => {
     return (
 
         <>
-            <h1 className={`player-info ${props.playerName === 'Player One' ? 'green' : 'pink'}`}>{props.playerName}</h1>
+            <h1 className={`player-info ${props.playerName === 'Player One' ? 'green' : 'pink'}`} id="player-one-label">{props.playerName}</h1>
             <h1 className={`player-info ${props.playerName === 'Player One' ? 'green' : 'pink'}`}>{props.symbol}</h1>
         </>
 

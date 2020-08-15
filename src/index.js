e2600cf7fb4744829d05b8abb90dea66e5184658
hineshvadgama <hinesh.vadgama@outlook.com>
2020-08-15 19:48:30 +0100

    reset app as main component to render

--- src/index.js
+++ src/index.js
@@ -2,6 +2,5 @@ import React from 'react';
 import { render } from 'react-dom';
 import './index.css';
 import App from './components/App/App.js';
-import WinnerModal from './components/WinnerModal/WinnerModal.js';
 
-render(<WinnerModal />, document.getElementById('app'));
+render(<App />, document.getElementById('app'));

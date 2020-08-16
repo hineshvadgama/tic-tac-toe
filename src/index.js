e4805f5adab3c4e1546cb80fa9e20b91e76ed57e
hineshvadgama <hinesh.vadgama@outlook.com>
2020-08-16 23:06:18 +0100

    auto update index.html

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from 'react';
 import { render } from 'react-dom';
 import './index.css';
 import App from './components/App/App.js';
+require('file-loader?name=[name].[ext]!./index.html');
 
 render(<App />, document.getElementById('app'));

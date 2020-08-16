8735b91d84f59e60f9cf2ad0145fcf64db537e83
hineshvadgama <hinesh.vadgama@outlook.com>
2020-08-16 23:09:01 +0100

    auto update favicon.ico

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import { render } from 'react-dom';
 import './index.css';
 import App from './components/App/App.js';
 require('file-loader?name=[name].[ext]!./index.html');
+require('file-loader?name=[name].[ext]!./favicon.ico');
 
 render(<App />, document.getElementById('app'));
